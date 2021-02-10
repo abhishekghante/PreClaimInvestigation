feb7d2b3d9542b5aaa2f499949bebd961d38c1ac
s.dixon <s.dixon@xangars.com>
2021-02-10 09:44:30 +0530

    Case Closure

--- pre-claim-investigation/src/main/java/com/preclaim/dao/Case_movementDaoImpl.java
+++ pre-claim-investigation/src/main/java/com/preclaim/dao/Case_movementDaoImpl.java
@@ -71,7 +71,7 @@ public class Case_movementDaoImpl implements Case_movementDao {
 		   query="INSERT INTO audit_case_movement(caseId, fromID, toId, caseStatus, remarks, createdDate, updatedDate) values(?, ?, ?, ?, '', getdate(), getdate()) ";
 		   this.template.update(query,caseMovement.getCaseId(), caseMovement.getFromId(), caseMovement.getToId(),caseMovement.getCaseStatus());
 			
-		   if(caseMovement.getCaseStatus() == "Closed")
+		   if(caseMovement.getCaseStatus().equals("Closed"))
 		   {
 			   query = "UPDATE case_lists SET caseStatus = ?, updatedBy = ?, updatedDate = getDate() where"
 			   		+ "caseId = ?";
